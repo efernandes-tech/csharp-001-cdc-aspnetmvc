69f32fc3199a67061f00a66d2b2577cef0672cbb
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-01-03 17:23:10 -0200

    1.1 e entao vamos comecar

---
+++ PrimeiraAplicacao/PrimeiraAplicacao/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrimeiraAplicacao.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Minha primeira aplicação com ASP.NET MVC 1";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Minha primeira aplicação com ASP.NET MVC 2";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Minha primeira aplicação com ASP.NET MVC 3";
+
+            return View();
+        }
+    }
+}
