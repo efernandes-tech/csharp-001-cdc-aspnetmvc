b73d8df96b247d5b30cfcf0bfb3b15df7c641995
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-07-18 17:04:15 -0300

    6.8 mobilidade: sua consulta de medicos em dispositivos moveis

--- CadeMeuMedico/CadeMeuMedico/App_Start/RouteConfig.cs
+++ CadeMeuMedico/CadeMeuMedico/App_Start/RouteConfig.cs
@@ -9,14 +9,42 @@ namespace CadeMeuMedico
 {
     public class RouteConfig
     {
+        // Alterando a rota padrão...
+        public static void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Mobile",
+                "Mobile/{controller}/{action}/{id}",
+                new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
+            );
+        }
+
+        // Dando prioridade para o controller de desktop.
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            /*routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );*/
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                },
+                // Adicione o namespace dos controladores desktop a seguir.
+                new[] { "CadeMeuMedico.Controllers" }
             );
         }
     }
---
+++ CadeMeuMedico/CadeMeuMedico/Areas/Mobile/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadeMeuMedico.Areas.Mobile.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Mobile/Home
+        public ActionResult Index()
+        {
+            return Content("teste");
+        }
+    }
+}
